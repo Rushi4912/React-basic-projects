0bb491d153f8f74b7f076b431815bce1fdfaee60
Rushi4912 <pawarrushikeshpawar@gmail.com>
2024-02-07 22:04:36 +0530

    change

--- brand-page/src/Components/Nav.jsx
+++ brand-page/src/Components/Nav.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Nav = () => {
   return (
        <div className='w-[333px] h-[24px] mt-[24px]  ' >
-            <ul className='flex flex-row gap-x-24 m-1'>
+            <ul className='flex flex-row gap-x-24 '>
                  
                  <li>Menu</li>
                  <li>Location</li>
