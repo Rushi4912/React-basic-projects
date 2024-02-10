125523db07af3719479628cdabde76bc329a40cd
Rushi4912 <pawarrushikeshpawar@gmail.com>
2024-02-10 22:21:37 +0530

    change

--- brand-page/src/Components/Header.jsx
+++ brand-page/src/Components/Header.jsx
@@ -2,7 +2,7 @@ import nike from '../assets/nike.png';
 import Nav from './Nav';
 const Header = () => {
   return (
-    <div className="flex flex-row h-[72px] justify-between w-[1440px] m-1">
+    <div className="flex flex-row h-[72px] justify-between w-[1440px]">
       <img
         src={nike}
         alt="nike image"
